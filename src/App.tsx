acf71dde18a1dac5af6e02c333cf49598b85fca5
2295ad <adarsh.nambiar22@gmail.com>
2021-09-27 16:01:09 +0530

    frst commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,31 @@
-import React from 'react';
-import logo from './logo.svg';
+import React,{useState, useEffect} from 'react';
 import './App.css';
+import { ThemeContext, Theme } from './ThemeContext';
+import MyPage from './MyPage';
+
 
 function App() {
+  const [theme, setTheme] = React.useState(Theme.Light);
+  const [data,setData] = useState(null);
+
+  useEffect(()=>{
+    fetch('./static/test.json').then((res)=>{
+      res.json().then((dt)=>{
+        setData(dt)
+      })
+    })
+  },[])
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ThemeContext.Provider value={{ theme, setTheme }}>
+      <div className="App">
+        <header className="App-header">
+          <MyPage />
+          {JSON.stringify(data)}
+        </header>
+      </div>
+    </ThemeContext.Provider>
   );
 }
 
